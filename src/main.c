e8df2fd85047c4053776d4183dabaae885fd6056
Kirill Fernandez <fernandes.kir@yandex.ru>
2022-05-11 12:28:47 +0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "stm8s_conf.h"
+/**
+  *inc->stm8s_conf.h - раскоментировать строчки с нужными хедерами 
+  *периферии и необходимыми обработчиками прерываний. Все закоментированные
+  *обработчики ведут на бесконечные циклы.
+  * 
+  *Project->Options->General Options - выбрать мк
+  *
+  *Project->Options->Debugger - выбрать отладчик
+  *
+  *Project->Options->C/C++ Compiler->Preprocessor->Defined symbols  - задать
+  *семейство процессора(перечислены в lib->SPL->inc->stm8s.h), а также задать
+  *частоты внутренних и внешних генераторов(если не задать, то будут ипользованы
+  *значения по умолчанию из stm8s.h).
+  */
+
+int SystemInit(void)
+{
+    return 0;
+}
+
+void main(void)
+{
+	SystemInit();
+	while (1){};
+}
+
