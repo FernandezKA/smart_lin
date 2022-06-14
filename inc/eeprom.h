d497cd5c7316ab0b014d78a9333525bd6ea26e30
Kirill Fernandez <fernandes.kir@yandex.ru>
2022-06-14 21:25:08 +0300

    Added structures for configuration packets

---
+++ inc/eeprom.h
@@ -0,0 +1,43 @@
+#ifndef _eeprom_h_
+#define _eeprom_h_
+#include "main.h"
+#include "stm8s_flash.h"
+
+#define EEPROM_INFO ((uint32_t) 0x004000)
+#define EEPROM_START_PACKET ((uint32_t) 0x004040)
+#define EEPROM_END_PACKET ((uint32_t) 0x004280)
+
+#define COUNT_PACKET 20U
+
+struct edges{
+  uint8_t edges_low[8U];
+  uint8_t edges_high[8U];
+};
+
+struct filer{
+  uint8_t PID;
+  uint8_t DLC;
+  uint8_t DATA[8U];
+  uint8_t CRC;
+  uint8_t timeout;
+};
+
+struct slave_packet{
+  uint8_t PID;
+  uint8_t DLC;
+  struct edges _edge[8U];
+  uint8_t diskr_out;
+  uint8_t CRC;
+};
+
+void get_receive(void);
+
+bool read_dev_info(uint8_t* pList);
+
+void write_dev_info(uint8_t* pList);
+
+bool read_pid_slave(uint8_t* pList);
+
+bool read_pid_filter(uint8_t* pList);
+
+#endif
---
+++ src/eeprom.c
@@ -0,0 +1,47 @@
+#include "eeprom.h"
+
+bool read_dev_info(uint8_t* pList){
+  bool status = false;
+  if(FLASH_ReadByte(EEPROM_INFO + sizeof(uint8_t)) != 0xFF){
+    status = true;
+    for(uint8_t i = 0; i < 0x08U; ++i){
+      pList[i] = FLASH_ReadByte(EEPROM_INFO +sizeof(uint8_t) * i);
+    }
+  }
+  return status;
+}
+
+void write_dev_info(uint8_t* pList)
+{
+  FLASH_Unlock(FLASH_MEMTYPE_DATA);
+  for(uint8_t i = 0; i < 0x08; ++i){
+    FLASH_ProgramByte(EEPROM_INFO + sizeof(uint8_t), pList[i]);
+  }
+  FLASH_Lock(FLASH_MEMTYPE_DATA);
+}
+
+bool read_pid_slave(uint8_t* pList)
+{
+  for(uint8_t i = 0; i < COUNT_PACKET; ++i){
+    if(FLASH_ReadByte(EEPROM_START_PACKET + sizeof(uint8_t) * i + 1U) != 0x01){ //Read byte RL_TYPE (Next from PID)
+      pList[i] = 0x00U;
+    }
+    else{ //If it's 0x01 type of packet, add to list with PID
+      pList[i] = FLASH_ReadByte(EEPROM_START_PACKET + sizeof(uint8_t) * i);
+    }
+  }
+  return true;
+}
+
+bool read_pid_filter(uint8_t* pList)
+{
+  for(uint8_t i = 0; i < COUNT_PACKET; ++i){
+    if(FLASH_ReadByte(EEPROM_START_PACKET + sizeof(uint8_t) * i + 1U) != 0x02){ //Read byte RL_TYPE (Next from PID)
+      pList[i] = 0x00U;
+    }
+    else{ //If it's 0x01 type of packet, add to list with PID
+      pList[i] = FLASH_ReadByte(EEPROM_START_PACKET + sizeof(uint8_t) * i);
+    }
+  }
+  return true;
+}
